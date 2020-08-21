636c48fde6c39d0e2578bc97aaf3e701a6ab8b0c
Faiz Hameed <faizftb@gmail.com>
2020-08-21 21:50:49 +0530

    useRef testing

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 function Hi() {
   const [state, setState] = React.useState({
-    name: "React",
+    name: "",
   });
-  const handleChange = () => {
-    setState((pre) => ({ ...pre, name: "Wow React" }));
+  const inputGreet = React.createRef((e) => {
+    console.log(e);
+  });
+  const greet = () => {
+    console.log("hi", inputGreet.current.value);
   };
-  React.useEffect(() => {
-    setState((pre) => ({ ...pre, name: "WaterMelon" }));
-  }, []);
+
+  const selectUpdate = (e) => {
+    setState({ ...state, name: e.target.value });
+  };
   return (
     <div>
       <p>This is a try</p>
-      <p>{state.name}</p>
-      <button onClick={handleChange}>click</button>
+      <select onChange={selectUpdate}>
+        <option>Faiz</option>
+        <option>Hameed</option>
+      </select>
+      <h1>Shown Name:{state.name}</h1>
+      <input ref={inputGreet} type="text" />
+      <button onClick={greet}>greet</button>
+      <Button inputRef={inputGreet} />
     </div>
   );
 }
-
+function Button({ inputRef }) {
+  const greet = () => {
+    console.log("hi", inputRef.current.value);
+  };
+  return <button onClick={greet}>click from </button>;
+}
 ReactDOM.render(
   <div>
     <h1>Learn React</h1>
